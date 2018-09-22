54e21a68af826f24b8e34c44f614e275a29eb746
Victor Lin <vlin96@gmail.com>
2018-09-22 10:14:11 -0400

    Update listings routing path
    per instructions: "use the listings router for requests going to the
    /api/listings path"
    and to be consistent with client/factories/listingFactory.js

--- server/routes/listings.server.routes.js
+++ server/routes/listings.server.routes.js
@@ -7,7 +7,7 @@ var listings = require('../controllers/listings.server.controller.js'),
   These method calls are responsible for routing requests to the correct request handler.
   Take note that it is possible for different controller functions to handle requests to the same route.
  */
-router.route('/')
+router.route('/listings')
   .get(listings.list)
   .post(listings.create);
 
