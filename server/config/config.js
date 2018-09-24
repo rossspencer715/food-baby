0587455d13f00b2995049c80f35d917e8467f8bb
Victor Lin <vlin96@gmail.com>
2018-09-24 19:06:03 -0400

    update port for Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  port: 8080,
+  port: process.env.PORT || 8080,
   db: {
-    // uri: 'mongodb://localhost:27017/listings'
     uri: 'mongodb://CEN3031:CEN3031TA@ds161062.mlab.com:61062/listings',
   }
 };
